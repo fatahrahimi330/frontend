9f2d42c087dc795479cc42da112b26b0025f563f
fatahrahimi330 <138868867+fatahrahimi330@users.noreply.github.com>
2024-02-25 23:10:11 +0600

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'https://backend-by48.onrender.com' : '/';
+  process.env.NODE_ENV !== 'production' ? 'https://backend-by48.onrender.com/' : '/';
